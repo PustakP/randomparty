31f04bc40ff755acb2e8b737d5f15abda890d3a5
rohan <rohan1706pandey@gmail.com>
2023-07-09 10:59:18 +0530

    added idle and jump animation
    jump animation images are still required but ive added the jump animation in the code. will make the images soon

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, os
 
 pygame.init()
 width = 900
@@ -10,6 +10,9 @@ pygame.display.set_caption('EXACTLY')
 clock = pygame.time.Clock()
 FPS = 60
 
+# define game variables
+GRAVITY = 0.75
+
 # define player action variables
 moving_left = False
 moving_right = False
@@ -18,6 +21,7 @@ k_key_held = False
 
 # define colors
 BG = (255, 255, 255)
+RED = (255, 0, 0)
 
 # Create a group to hold all sprites
 all_sprites = pygame.sprite.Group()
@@ -32,17 +36,32 @@ red_rect = pygame.Rect(red_rect_position[0],red_rect_position[1],50,50)
 class Platformer(pygame.sprite.Sprite):
     def __init__(self, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
+        self.alive = True
         self.speed = speed
         self.direction = 1
+        self.vel_y = 0
+        self.jump = False
+        self.in_air = True
         self.flip = False
         self.animation_list = []
         self.frame_index = 0
+        self.action = 0
         self.update_time = pygame.time.get_ticks()
-        for i in range(8):
-            img = pygame.image.load(f'img/player/run/{i}.png')
-            img = pygame.transform.scale(img, (img.get_width() * scale, img.get_height() * scale))
-            self.animation_list.append(img)
-        self.image = self.animation_list[self.frame_index]
+
+        # load all images for the players
+        animation_types = ['idle', 'run', 'jump']
+        for animation in animation_types:
+            # reset temp list of images
+            temp_list = []
+            # count number of files in the folder
+            num_of_frames = len(os.listdir(f'img/player/{animation}'))
+            for i in range(num_of_frames):
+                img = pygame.image.load(f'img/player/{animation}/{i}.png').convert_alpha()
+                img = pygame.transform.scale(img, (img.get_width() * scale, img.get_height() * scale))
+                temp_list.append(img)
+            self.animation_list.append(temp_list)
+
+        self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
@@ -60,6 +79,24 @@ class Platformer(pygame.sprite.Sprite):
             self.flip = False
             self.direction = -1
 
+        # jump
+        if self.jump and self.in_air == False:
+            self.vel_y = -11
+            self.jump = False
+            self.in_air = True
+
+        # apply gravity
+        self.vel_y += GRAVITY
+        if self.vel_y > 10:
+            self.vel_y = 10
+        dy += self.vel_y
+
+        # check collision with floor
+        if self.rect.bottom + dy > 600:
+            dy = 600 - self.rect.bottom
+            self.in_air = False
+
+
         # update rectangle postition
         self.rect.x += dx
         self.rect.y += dy
@@ -68,27 +105,46 @@ class Platformer(pygame.sprite.Sprite):
         # update animation
         ANIMATION_COOLDOWN = 100
         # update image depending on current frame
-        self.image = self.animation_list[self.frame_index]
+        self.image = self.animation_list[self.action][self.frame_index]
         # check if enough time has passed since the last update
         if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
             self.update_time = pygame.time.get_ticks()
             self.frame_index += 1
         # if the animation has run out of frames, then reset back to start
-        if self.frame_index >= len(self.animation_list):
+        if self.frame_index >= len(self.animation_list[self.action]):
             self.frame_index = 0
+
+    def update_action(self, new_action):
+        # check if the new action is different to the previous one
+        if new_action != self.action:
+            self.action = new_action
+            # update the animation settings
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
 
     def draw(self):
         screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
-player = Platformer(200, 200, 2, 7) # x postion, y postition, scale, speed
+player = Platformer(200, 200, 1.9, 7) # x postion, y postition, scale, speed
 
 run = True
 while run:
     clock.tick(FPS)
     screen.fill(BG)
+    pygame.draw.line(screen, RED, (0, 600), (width, 600)) # draws a line for floor (temparary)
     player.update_animation()
     player.draw() # draws the platformer on screen
-    player.move(moving_left, moving_right)
+
+    # update player actions
+    if player.alive:
+        if player.in_air:
+            player.update_action(2)  # 2: jumping
+        elif moving_left or moving_right:
+            player.update_action(1) # 1 means run
+        else:
+            player.update_action(0) # 0 means idle
+        player.move(moving_left, moving_right)
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -99,6 +155,8 @@ while run:
                 moving_left = True
             if event.key == pygame.K_d:
                 moving_right = True
+            if event.key == pygame.K_SPACE and player.alive:
+                player.jump = True
             if event.key == pygame.K_ESCAPE:
                 run = False
             if event.key == pygame.K_k and toggleControl:  # if "K"is pressed and togglecontrol is True
